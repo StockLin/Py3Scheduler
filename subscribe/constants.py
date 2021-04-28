3289ee6469a1a9badd8e3891ef7547666c2d58a3
DESKTOP-8OQB9FB\liang <liangyun.stark@gmail.com>
2021-04-28 22:15:44 +0800

    set pre-define variable.

---
+++ subscribe/constants.py
@@ -0,0 +1,16 @@
+STATE_DICT = {
+    0:"STOPPED",
+    1:"RUNNING",
+    2:"PAUSED"
+}
+
+# pre-define trigger period constants
+HOURLY_PERIOD = "H"
+DAILY_PERIOD = "D"
+WEEKLY_PERIOD = "W"
+MONTHLY_PERIOD = "M"
+
+# Apscheudler status
+STATE_STOPPED = 0
+STATE_RUNNING = 0
+STATE_PAUSED = 0
